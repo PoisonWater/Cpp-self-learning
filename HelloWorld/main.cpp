823df10d99bee031c181c9014abb8846d5455f32
Yichang Liu <39092148+PoisonWater@users.noreply.github.com>
2018-05-20 15:11:29 +0800

    Cpp Day 1.1
    Hello, world!

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout << "Hello world!\nWhat is your name?\n";
+    cin >> name;
+    cout << "How old are you then?\n";
+    cin >> age;
+    cout << "I am " << name << " and I am " << age << " years old.\n";
+    return 0;
+}
