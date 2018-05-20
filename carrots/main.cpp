39065bfb89dc1a998ba0e23c6d169eef39afcab9
Yichang Liu <39092148+PoisonWater@users.noreply.github.com>
2018-05-20 15:08:09 +0800

    Cpp Day 1
    The second program of my cpp programming after the "Hello, World!".

---
+++ carrots/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  carrots
+//
+//  Created by 刘亦畅 on 2018/5/20.
+//  Copyright © 2018年 刘亦畅. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int carrots;
+    carrots = 25;
+    cout << "I have " << carrots << " carrots." << endl;
+    carrots -= 1;
+    cout << "Oh no, I have " << carrots << " carrots now." << endl;
+    return 0;
+}
